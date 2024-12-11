5c40e2f340fcd233628f553115e645a10f8f12d2
steppenwolffenrir <steppenwolffenrir@gmail.com>
2024-12-12 02:44:13 +0300

    chore: improve file structure

---
+++ challenges/fundamentals-pt1/challenge-3.js
@@ -0,0 +1,26 @@
+/*
+
+Coding Challenge #3
+
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+1. 2. 3. 4. Calculate the average score for each team, using the test data below
+Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks 😉
+Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+*/
---
+++ lectures/fundamentals-pt1/values and variables.js
@@ -0,0 +1,13 @@
+/*
+
+LECTURE: Values and Variables
+
+1.Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2.Log their values to the console
+
+ */
+
+let country = 'Russia';
+let continent = 'South Europe';
+let population = 146;
